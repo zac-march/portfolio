e958a7f3cd61dea0770a1b8984a32cded80fd5f3
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-12 12:47:14 +1000

    Center a div

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,12 +4,14 @@ import React from "react";
 function Hero() {
   return (
     <section className={style.container}>
-      <h1>Let's work together.</h1>
-      <p>
-        I'm a dedicated Front-End Developer based in Melbourne, Australia. With
-        a passion for crafting immersive and user-friendly web experiences, I
-        thrive on tackling development challenges head-on.
-      </p>
+      <div className={style.wrapper}>
+        <h1>Let's work together.</h1>
+        <p>
+          I'm a dedicated Front-End Developer based in Melbourne, Australia.
+          With a passion for crafting immersive and user-friendly web
+          experiences, I thrive on tackling development challenges head-on.
+        </p>
+      </div>
     </section>
   );
 }
