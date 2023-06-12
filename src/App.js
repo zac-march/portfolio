79afa7f1aec491ef47291a90871b1f923c630f28
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-12 12:17:04 +1000

    Integrate Nav and Hero components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Nav from "./components/Nav/Nav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
 }
 
 export default App;
