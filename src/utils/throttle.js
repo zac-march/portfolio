29dcf87a12ebbc293157ed0db0d6c96b3346b73c
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-15 08:21:42 +1000

    Add event throttling utility function

---
+++ src/utils/throttle.js
@@ -0,0 +1,16 @@
+function throttle(func, wait) {
+  let waiting = false;
+  return function () {
+    if (waiting) {
+      return;
+    }
+
+    waiting = true;
+    setTimeout(() => {
+      func.apply(this, arguments);
+      waiting = false;
+    }, wait);
+  };
+}
+
+export default throttle;
